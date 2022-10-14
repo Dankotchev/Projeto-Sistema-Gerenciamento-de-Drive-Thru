4c5b6abd68f4a978e4bec37353b17f373d4a4537
Dankotchev <dankotchev.tech@outlook.com>
2022-10-14 20:50:53 -0300

    Principal para testes & AbstractDAO

---
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/PrincipalTestes.java
@@ -0,0 +1,5 @@
+package br.edu.ifsp.pep.projetointegrador.sgdt;
+
+public class PrincipalTestes {
+
+}
---
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/controledao/AbstractDAO.java
@@ -0,0 +1,50 @@
+package br.edu.ifsp.pep.projetointegrador.sgdt.controledao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class AbstractDAO<T> {
+
+    private final EntityManagerFactory emf;
+
+    public AbstractDAO() {
+        emf = Persistence
+                .createEntityManagerFactory("ProjetoIntegradorSGDT_PU");
+    }
+
+    protected EntityManager getEntityManager() {
+        return this.emf.createEntityManager();
+    }
+
+    public void inserir(T entity) throws Exception {
+        EntityManager em = getEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(entity);
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public void alterar(T entity) {
+        EntityManager em = getEntityManager();
+
+        em.getTransaction().begin();
+        em.merge(entity);
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public void remover(T entity) {
+        EntityManager em = getEntityManager();
+
+        em.getTransaction().begin();
+        em.remove(em.merge(entity));
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+}
