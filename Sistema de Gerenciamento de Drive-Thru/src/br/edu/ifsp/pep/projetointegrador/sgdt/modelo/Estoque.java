0880bc22dbae850b3a25dd2675b637ddab675d12
Dankotchev <dankotchev.tech@outlook.com>
2022-10-16 12:02:31 -0300

    Modelo: Estoque

---
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/modelo/Estoque.java
@@ -0,0 +1,62 @@
+package br.edu.ifsp.pep.projetointegrador.sgdt.modelo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estoque")
+public class Estoque implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    
+    @Column(name = "descricao", length = 60)
+    private String descricao;
+    
+    @Column(name = "quantidade")
+    private Integer quantidade;
+    
+    @Column(name = "custo_unitario", precision = 10, scale = 2)
+    private BigDecimal custoUnitario;
+    
+    //  Código Gerado
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getCustoUnitario() {
+        return custoUnitario;
+    }
+
+    public void setCustoUnitario(BigDecimal custoUnitario) {
+        this.custoUnitario = custoUnitario;
+    }
+}
