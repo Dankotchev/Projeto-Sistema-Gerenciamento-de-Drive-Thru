dd2086e85673ef1b963e7885eed09605f056f1ff
Dankotchev <dankotchev.tech@outlook.com>
2022-10-20 14:58:44 -0300

    Modelo: Produto

---
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/modelo/Produto.java
@@ -0,0 +1,73 @@
+package br.edu.ifsp.pep.projetointegrador.sgdt.modelo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    
+    @Column(name = "nome_produto", length = 60)
+    private String nomeProduto;
+    
+    @Column(name = "preco_unitario_produto", precision = 10, scale = 2)
+    private BigDecimal precoUnitarioProduto;
+    
+    @Column(name = "descricao_produto", length = 100)
+    private String descricaoProduto;
+    
+    @Column(name =  "quantidade_produto")
+    private Integer quantidadeProduto;
+    
+    //  Código Gerado
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getPrecoUnitarioProduto() {
+        return precoUnitarioProduto;
+    }
+
+    public void setPrecoUnitarioProduto(BigDecimal precoUnitarioProduto) {
+        this.precoUnitarioProduto = precoUnitarioProduto;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public Integer getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+
+    public void setQuantidadeProduto(Integer quantidadeProduto) {
+        this.quantidadeProduto = quantidadeProduto;
+    }
+}
