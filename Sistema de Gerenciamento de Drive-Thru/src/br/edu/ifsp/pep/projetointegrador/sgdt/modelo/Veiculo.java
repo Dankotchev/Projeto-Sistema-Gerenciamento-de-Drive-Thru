6d50b8c43501b516ef3a7135e5655eb8f93596e7
Dankotchev <dankotchev.tech@outlook.com>
2022-10-20 20:12:34 -0300

    Veículo: Named Queries buscar todos e buscar por placa

--- Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/modelo/Veiculo.java
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/modelo/Veiculo.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -16,6 +18,12 @@ import javax.persistence.UniqueConstraint;
     @UniqueConstraint(
             name = "unique_id_placa",
             columnNames = {"id", "placa"})
+})
+@NamedQueries(value = {
+    @NamedQuery(name = "Veiculo.buscarTodos",
+            query = "SELECT v FROM Veiculo v"),
+    @NamedQuery(name = "Veiculo.buscarPorPlaca",
+            query = "SELECT v FROM Veiculo v WHERE v.placa = :placa")
 })
 public class Veiculo implements Serializable {
 
