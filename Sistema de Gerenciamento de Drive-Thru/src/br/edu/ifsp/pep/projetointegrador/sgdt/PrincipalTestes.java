82793b44c7ef4aeae8896de8b47f04d5ec863ace
Dankotchev <dankotchev.tech@outlook.com>
2022-10-20 18:31:24 -0300

    ControleDAO: Veículo e testes

--- Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/PrincipalTestes.java
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/PrincipalTestes.java
@@ -1,5 +1,21 @@
 package br.edu.ifsp.pep.projetointegrador.sgdt;
 
+import br.edu.ifsp.pep.projetointegrador.sgdt.controledao.VeiculoDAO;
+import br.edu.ifsp.pep.projetointegrador.sgdt.modelo.Veiculo;
+
 public class PrincipalTestes {
 
+    public static void main(String[] args) {
+
+        Veiculo veiculo = new Veiculo();
+        VeiculoDAO veiculoDAO = new VeiculoDAO();
+        veiculo.setPlaca("ABC1E23");
+
+        try {
+            veiculoDAO.inserir(veiculo);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
 }
---
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/controledao/VeiculoDAO.java
@@ -0,0 +1,7 @@
+package br.edu.ifsp.pep.projetointegrador.sgdt.controledao;
+
+import br.edu.ifsp.pep.projetointegrador.sgdt.modelo.Veiculo;
+
+public class VeiculoDAO extends AbstractDAO<Veiculo>{
+    
+}
