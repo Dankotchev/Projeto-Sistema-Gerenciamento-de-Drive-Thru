467452d6c5d7a8d173036a46d7ed77caba835e29
Dankotchev <dankotchev.tech@outlook.com>
2022-10-20 15:14:56 -0300

    Modelo: PedidoProduto

---
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/modelo/PedidoProduto.java
@@ -0,0 +1,66 @@
+package br.edu.ifsp.pep.projetointegrador.sgdt.modelo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedido_produto")
+@IdClass(PedidoProdutoPK.class)
+public class PedidoProduto implements Serializable {
+    
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedido pedido;
+    
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Produto produto;
+    
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantidade;
+    
+    @Column(name = "preco_unitario_produto", precision = 10, scale = 2, nullable = false)
+    private BigDecimal precoUnitarioProduto;
+    
+    //  Código Gerado
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoUnitarioProduto() {
+        return precoUnitarioProduto;
+    }
+
+    public void setPrecoUnitarioProduto(BigDecimal precoUnitarioProduto) {
+        this.precoUnitarioProduto = precoUnitarioProduto;
+    }
+}
