894a19807fc8dec5019f0a107e348ca1418c2df9
Dankotchev <dankotchev.tech@outlook.com>
2022-11-09 17:19:13 -0300

    Funcionalidades dos botões Inserir, Visualizar, Cancelar, Alterar e Excluir

--- Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/visao/RefeicaoVisao.java
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/sgdt/visao/RefeicaoVisao.java
@@ -8,26 +8,29 @@ import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
 public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeTela<Refeicao> {
-    
-    private List<Refeicao> listaRefeicoes;
+
+    private List<Refeicao> listagemDeRefeicoes;
+    private Refeicao refeicaoGlobal;
     private final RefeicaoDAO refeicaoDAO = new RefeicaoDAO();
-    
+
     public RefeicaoVisao(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
         this.setLocationRelativeTo(null);
         this.setEstadoBotoes(true);
-        this.setEstadoCamposTexto(false);
+        this.setEstadoCamposTexto(true);
+        this.setVisibilidadeCamposTextos(false);
+        this.btnOK.setVisible(false);
     }
-    
+
     private void atualizarTabela() {
-        if (this.listaRefeicoes.isEmpty()) {
+        if (this.listagemDeRefeicoes.isEmpty()) {
             Mensagem.mAviso("Não há Refeições cadastradas");
         } else {
             DefaultTableModel modelo = (DefaultTableModel) this.tabelaRefeicao.getModel();
             modelo.setNumRows(0);
-            
-            for (Refeicao refeicao : listaRefeicoes) {
+
+            for (Refeicao refeicao : this.listagemDeRefeicoes) {
                 modelo.addRow(new Object[]{refeicao.getNomeRefeicao(), refeicao.getPrecoUnitarioRefeicao(),
                     refeicao.getDescricaoRefeicao(), refeicao.getListaIngredientes()
                 });
@@ -65,7 +68,8 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
         txtIngrediente = new javax.swing.JTextField();
         btnPesquisar = new javax.swing.JButton();
         labelPreco = new javax.swing.JLabel();
-        txtValor = new javax.swing.JTextField();
+        txtPreco = new javax.swing.JTextField();
+        btnOK = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Manter Refeição");
@@ -81,36 +85,66 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
         btnInserir.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnInserir.setForeground(new java.awt.Color(217, 28, 38));
         btnInserir.setText("Inserir");
+        btnInserir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnInserirActionPerformed(evt);
+            }
+        });
         painelBotoes.add(btnInserir);
 
         btnVisualizar.setBackground(new java.awt.Color(243, 192, 32));
         btnVisualizar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnVisualizar.setForeground(new java.awt.Color(217, 28, 38));
         btnVisualizar.setText("Visualizar");
+        btnVisualizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVisualizarActionPerformed(evt);
+            }
+        });
         painelBotoes.add(btnVisualizar);
 
         btnGravar.setBackground(new java.awt.Color(243, 192, 32));
         btnGravar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnGravar.setForeground(new java.awt.Color(217, 28, 38));
         btnGravar.setText("Gravar");
+        btnGravar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGravarActionPerformed(evt);
+            }
+        });
         painelBotoes.add(btnGravar);
 
         btnCancelar.setBackground(new java.awt.Color(243, 192, 32));
         btnCancelar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnCancelar.setForeground(new java.awt.Color(217, 28, 38));
         btnCancelar.setText("Cancelar");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
         painelBotoes.add(btnCancelar);
 
         btnAlterar.setBackground(new java.awt.Color(243, 192, 32));
         btnAlterar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnAlterar.setForeground(new java.awt.Color(217, 28, 38));
         btnAlterar.setText("Alterar");
+        btnAlterar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAlterarActionPerformed(evt);
+            }
+        });
         painelBotoes.add(btnAlterar);
 
         btnExcluir.setBackground(new java.awt.Color(243, 192, 32));
         btnExcluir.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnExcluir.setForeground(new java.awt.Color(217, 28, 38));
         btnExcluir.setText("Excluir");
+        btnExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExcluirActionPerformed(evt);
+            }
+        });
         painelBotoes.add(btnExcluir);
 
         painelTitulo.setBackground(new java.awt.Color(243, 192, 32));
@@ -146,7 +180,7 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
 
         tabelaRefeicao.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null}
+
             },
             new String [] {
                 "Nome", "Preço Unit.", "Descriçao", "Lista de Ingredientes"
@@ -178,6 +212,16 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
         labelPreco.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         labelPreco.setText("Preço:");
 
+        btnOK.setBackground(new java.awt.Color(243, 192, 32));
+        btnOK.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnOK.setForeground(new java.awt.Color(217, 28, 38));
+        btnOK.setText("OK");
+        btnOK.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOKActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout painelFundoLayout = new javax.swing.GroupLayout(painelFundo);
         painelFundo.setLayout(painelFundoLayout);
         painelFundoLayout.setHorizontalGroup(
@@ -192,15 +236,17 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
                             .addComponent(jScrollPane6)
                             .addComponent(painelBotoes, javax.swing.GroupLayout.DEFAULT_SIZE, 854, Short.MAX_VALUE)
                             .addGroup(painelFundoLayout.createSequentialGroup()
-                                .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                    .addComponent(labelDescricao, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
-                                    .addComponent(labelNome, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(labelDescricao, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
+                                    .addComponent(labelNome, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(txtDescricao, javax.swing.GroupLayout.DEFAULT_SIZE, 365, Short.MAX_VALUE)
                                     .addComponent(txtNome, javax.swing.GroupLayout.DEFAULT_SIZE, 365, Short.MAX_VALUE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(btnPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(btnOK, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(painelFundoLayout.createSequentialGroup()
                                 .addComponent(labelIngredientes, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -210,7 +256,7 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
                 .addContainerGap()
                 .addComponent(labelPreco, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtValor, javax.swing.GroupLayout.PREFERRED_SIZE, 365, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txtPreco, javax.swing.GroupLayout.PREFERRED_SIZE, 365, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         painelFundoLayout.setVerticalGroup(
@@ -224,7 +270,8 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
                     .addComponent(labelNome, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(btnPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btnPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnOK, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(txtDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -236,7 +283,7 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
                             .addComponent(labelIngredientes, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(painelFundoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtValor, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtPreco, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(labelPreco, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(39, 39, 39)
                 .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -260,13 +307,75 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarActionPerformed
-        if (!this.txtNome.equals("")) {
-            this.listaRefeicoes = refeicaoDAO.buscarPorNome(this.txtNome.getText());
-            this.atualizarTabela();
-        } else {
-            Mensagem.mAviso("Informe um nome para a busca");
-        }
+        this.listagemDeRefeicoes = refeicaoDAO.buscarPorNome(this.txtNome.getText());
+        this.atualizarTabela();
+        this.setEstadoBotoes(true);
     }//GEN-LAST:event_btnPesquisarActionPerformed
+
+    private void btnInserirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInserirActionPerformed
+        this.limparCampos();
+        this.setVisibilidadeCamposTextos(true);
+        this.txtNome.requestFocus();
+        this.setEstadoBotoes(false);
+        this.setEstadoCamposTexto(true);
+    }//GEN-LAST:event_btnInserirActionPerformed
+
+    private void btnVisualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVisualizarActionPerformed
+        if (this.tabelaRefeicao.getSelectedRow() >= 0) {
+            this.setCampos(this.listagemDeRefeicoes
+                    .get(this.tabelaRefeicao.getSelectedRow()));
+            this.setVisibilidadeCamposTextos(true);
+            this.setEstadoCamposTexto(false);
+            this.btnOK.setVisible(true);
+        } else {
+            Mensagem.mAviso("Nenhuma Refeição selecionada.\nSelecione uma refeição");
+        }
+    }//GEN-LAST:event_btnVisualizarActionPerformed
+
+    private void btnOKActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOKActionPerformed
+        this.limparCampos();
+        this.setEstadoCamposTexto(true);
+        this.setVisibilidadeCamposTextos(false);
+        this.btnOK.setVisible(false);
+    }//GEN-LAST:event_btnOKActionPerformed
+
+    private void btnAlterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAlterarActionPerformed
+        if (this.tabelaRefeicao.getSelectedRow() >= 0) {
+            this.refeicaoGlobal = this.listagemDeRefeicoes
+                    .get(this.tabelaRefeicao.getSelectedRow());
+            this.setCampos(this.refeicaoGlobal);
+            this.setVisibilidadeCamposTextos(true);
+            this.setEstadoCamposTexto(true);
+            this.setEstadoBotoes(false);
+        } else {
+            Mensagem.mAviso("Nenhuma Refeição selecionada.\nSelecione uma refeição");
+        }
+    }//GEN-LAST:event_btnAlterarActionPerformed
+
+    private void btnExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExcluirActionPerformed
+        if (this.tabelaRefeicao.getSelectedRow() >= 0) {
+            this.refeicaoGlobal = this.listagemDeRefeicoes
+                    .get(this.tabelaRefeicao.getSelectedRow());
+            this.setCampos(this.refeicaoGlobal);
+            this.setVisibilidadeCamposTextos(true);
+            this.setEstadoCamposTexto(false);
+            this.setEstadoBotoes(false);
+            this.btnGravar.setText("Excluir");
+        } else {
+            Mensagem.mAviso("Nenhuma Refeição selecionada.\nSelecione uma refeição");
+        }
+    }//GEN-LAST:event_btnExcluirActionPerformed
+
+    private void btnGravarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGravarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnGravarActionPerformed
+
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        this.setEstadoBotoes(true);
+        this.setEstadoCamposTexto(true);
+        this.setVisibilidadeCamposTextos(false);
+        this.limparCampos();
+    }//GEN-LAST:event_btnCancelarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -316,6 +425,7 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
     private javax.swing.JButton btnExcluir;
     private javax.swing.JButton btnGravar;
     private javax.swing.JButton btnInserir;
+    private javax.swing.JButton btnOK;
     private javax.swing.JButton btnPesquisar;
     private javax.swing.JButton btnVisualizar;
     private javax.swing.JScrollPane jScrollPane6;
@@ -332,7 +442,7 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
     private javax.swing.JTextField txtDescricao;
     private javax.swing.JTextField txtIngrediente;
     private javax.swing.JTextField txtNome;
-    private javax.swing.JTextField txtValor;
+    private javax.swing.JTextField txtPreco;
     // End of variables declaration//GEN-END:variables
 
     @Override
@@ -355,25 +465,38 @@ public class RefeicaoVisao extends javax.swing.JDialog implements UtilitariosDeT
         this.btnGravar.setVisible(!estado);
         this.btnCancelar.setVisible(!estado);
     }
-    
+
     @Override
     public void setEstadoCamposTexto(boolean estado) {
-        this.txtNome.setEditable(estado);
-        this.txtDescricao.setEditable(estado);
-        this.txtIngrediente.setEditable(estado);
-        this.txtValor.setEditable(estado);
+        this.txtNome.setEnabled(estado);
+        this.txtDescricao.setEnabled(estado);
+        this.txtIngrediente.setEnabled(estado);
+        this.txtPreco.setEnabled(estado);
     }
-    
+
+    @Override
+    public void setVisibilidadeCamposTextos(boolean estado) {
+        this.txtDescricao.setVisible(estado);
+        this.txtIngrediente.setVisible(estado);
+        this.txtPreco.setVisible(estado);
+        this.labelDescricao.setVisible(estado);
+        this.labelIngredientes.setVisible(estado);
+        this.labelPreco.setVisible(estado);
+    }
+
     @Override
     public void limparCampos() {
         this.txtNome.setText("");
         this.txtDescricao.setText("");
         this.txtIngrediente.setText("");
-        this.txtValor.setText("");
+        this.txtPreco.setText("");
     }
-    
+
     @Override
-    public void setCamposCliente(Refeicao entity) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void setCampos(Refeicao entity) {
+        this.txtNome.setText(entity.getNomeRefeicao());
+        this.txtDescricao.setText(entity.getDescricaoRefeicao());
+        this.txtIngrediente.setText(entity.getListaIngredientes());
+        this.txtPreco.setText(String.valueOf(entity.getPrecoUnitarioRefeicao()));
     }
 }
--- Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/utilitarios/UtilitariosDeTela.java
+++ Sistema de Gerenciamento de Drive-Thru/src/br/edu/ifsp/pep/projetointegrador/utilitarios/UtilitariosDeTela.java
@@ -5,8 +5,10 @@ public interface UtilitariosDeTela<T> {
     public void setEstadoBotoes(boolean estado);
 
     public void setEstadoCamposTexto(boolean estado);
+    
+    public void setVisibilidadeCamposTextos(boolean estado);
 
     public void limparCampos();
 
-    public void setCamposCliente(T entity);
+    public void setCampos(T entity);
 }
